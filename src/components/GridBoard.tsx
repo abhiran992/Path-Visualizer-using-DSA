6d25409a43cba0d0b8ed5423507fb47b65317e3b
abhiran992 <abhiran992@gmail.com>
2024-07-05 01:30:58 +0530

    commit

--- src/components/GridBoard.tsx
+++ src/components/GridBoard.tsx
@@ -297,7 +297,7 @@ const GridBoard = () => {
           />
 
           <a
-            href={"https://github.com/Berntalz/DSA_pathVisualizer"}
+            href={"https://github.com/abhiran992/Path-Visualizer-using-DSA"}
             target="_blank"
             className="items-center w-fit disabled:bg-gray-400 disabled:cursor-not-allowed inline-flex bg-gray-600 text-[15px] text-white px-4 py-2 rounded-md"
           >
